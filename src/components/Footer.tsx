a20824d0792ddd66435fc41557d571b0cae3ad74
Noel Tran <dinhanhduy.tran@gmail.com>
2025-08-27 17:07:52 +1000

    make footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-rose-200 dark:bg-gray-800 py-6 mt-auto">
+      <div className="max-w-6xl mx-auto px-6 text-center">
+        <p className="text-slate-900 dark:text-gray-300">
+          © Noel Tran - Student No: 21654197 | Date: 27/08/2025
+        </p>
+      </div>
+    </footer>
+  );
+}
