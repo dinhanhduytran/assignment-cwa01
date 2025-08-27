18cdbb2b6f65cc6afdc7b3feff20467b06b67420
Noel Tran <dinhanhduy.tran@gmail.com>
2025-08-27 16:15:14 +1000

    fix the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,21 @@
-import { ModeToggle } from "./ThemeToggle"
+import { ModeToggle } from "./ThemeToggle";
+
 export default function Header() {
-    return (
-        <div className="flex-row flex justify-between items-center p-4 border-b-2  bg-rose-50 dark:bg-slate-900 ">
-            {/* Left side: Name and ID */}
-            <span className="border-2 rounded-md px-4 py-1 shadow-sm font-semibold">
-                <span className="hidden sm:inline">Dinh Anh Duy Tran - 21654197</span>
-                <span className="sm:hidden">21654197</span>
-            </span>
-            {/* Middle: App Name */}
-            <div className="absolute left-1/2 top-4 -translate-x-1/2">
-                <span className=" text-3xl font-bold">Tab Generator</span>
-            </div>
-            {/* Right side: Theme Toggle */}
-            <ModeToggle />
-        </div>
-    )
+  return (
+    <div className="flex justify-between items-center p-4 border-b-2 bg-rose-50 dark:bg-slate-900 relative">
+      {/* Left side: Name and ID */}
+      <span className="rounded-md px-4 py-1 shadow-sm font-semibold bg-rose-500 hover:bg-rose-600 text-white">
+        <span className="hidden md:inline">Dinh Anh Duy Tran - 21654197</span>
+        <span className="md:hidden">21654197</span>
+      </span>
+
+      {/* Middle: App Name */}
+      <div className="hidden md:inline absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <span className="text-3xl font-bold text-rose-500">Tab Generator</span>
+      </div>
+
+      {/* Right side: Theme Toggle */}
+      <ModeToggle />
+    </div>
+  );
 }
